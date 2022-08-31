2600dff5e3cd6e1932488abe1dce09663c17a005
Trim21 <trim21.me@gmail.com>
2022-08-31 20:57:41 +0800

    replace `Run` with `RunRewrite`

--- gofmt/golangci.go
+++ gofmt/golangci.go
@@ -6,46 +6,12 @@ import (
 	"go/ast"
 	"go/parser"
 	"go/printer"
-	"io/ioutil"
 	"os"
 	"strings"
 )
 
 func Run(filename string, needSimplify bool) ([]byte, error) {
-	src, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return nil, err
-	}
-
-	initParserMode()
-
-	file, sourceAdj, indentAdj, err := parse(fileSet, filename, src, false)
-	if err != nil {
-		return nil, err
-	}
-
-	ast.SortImports(fileSet, file)
-
-	if needSimplify {
-		simplify(file)
-	}
-
-	res, err := format(fileSet, file, sourceAdj, indentAdj, src, printer.Config{Mode: printerMode, Tabwidth: tabWidth})
-	if err != nil {
-		return nil, err
-	}
-
-	if bytes.Equal(src, res) {
-		return nil, nil
-	}
-
-	// formatting has changed
-	data, err := diff(src, res, filename)
-	if err != nil {
-		return nil, fmt.Errorf("error computing diff: %s", err)
-	}
-
-	return data, nil
+	return RunRewrite(filename, needSimplify, "")
 }
 
 func RunRewrite(filename string, needSimplify bool, rewrite string) ([]byte, error) {
