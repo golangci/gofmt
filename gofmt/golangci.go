36cf0231377da6f9a049957d24ed6b8a808e446f
Trim21 <trim21.me@gmail.com>
2022-08-31 19:12:15 +0800

    add RunRewrite

--- gofmt/golangci.go
+++ gofmt/golangci.go
@@ -4,8 +4,11 @@ import (
 	"bytes"
 	"fmt"
 	"go/ast"
+	"go/parser"
 	"go/printer"
 	"io/ioutil"
+	"os"
+	"strings"
 )
 
 func Run(filename string, needSimplify bool) ([]byte, error) {
@@ -43,4 +46,80 @@ func Run(filename string, needSimplify bool) ([]byte, error) {
 	}
 
 	return data, nil
+}
+
+func RunRewrite(filename string, needSimplify bool, rewrite string) ([]byte, error) {
+	rewriter, err := getRewrite(rewrite)
+	if err != nil {
+		return nil, err
+	}
+
+	src, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	initParserMode()
+
+	file, sourceAdj, indentAdj, err := parse(fileSet, filename, src, false)
+	if err != nil {
+		return nil, err
+	}
+
+	ast.SortImports(fileSet, file)
+
+	if needSimplify {
+		simplify(file)
+	}
+
+	if rewriter != nil {
+		file = rewriter(file)
+	}
+
+	res, err := format(fileSet, file, sourceAdj, indentAdj, src, printer.Config{Mode: printerMode, Tabwidth: tabWidth})
+	if err != nil {
+		return nil, err
+	}
+
+	if bytes.Equal(src, res) {
+		return nil, nil
+	}
+
+	// formatting has changed
+	data, err := diff(src, res, filename)
+	if err != nil {
+		return nil, fmt.Errorf("error computing diff: %s", err)
+	}
+
+	return data, nil
+}
+
+func getRewrite(rewriteRule string) (func(*ast.File) *ast.File, error) {
+	if rewriteRule == "" {
+		return nil, nil
+	}
+
+	f := strings.Split(rewriteRule, "->")
+	if len(f) != 2 {
+		return nil, fmt.Errorf("rewrite rule must be of the form 'pattern -> replacement'\n")
+	}
+	pattern, err := parseExprErr(f[0], "pattern")
+	if err != nil {
+		return nil, err
+	}
+
+	replace, err := parseExprErr(f[1], "replacement")
+	if err != nil {
+		return nil, err
+	}
+
+	return func(p *ast.File) *ast.File { return rewriteFile(pattern, replace, p) }, nil
+}
+
+func parseExprErr(s, what string) (ast.Expr, error) {
+	x, err := parser.ParseExpr(s)
+	if err != nil {
+		return nil, fmt.Errorf("parsing %s %s at %s\n", what, s, err)
+	}
+	return x, nil
 }
