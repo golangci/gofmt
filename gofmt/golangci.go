fadf1342bc289318214a4b9244315150a32011ee
Trim21 <trim21me@gmail.com>
2022-09-01 02:49:38 +0800

    feat: add rewrite option (#2)
    Co-authored-by: Fernandez Ludovic <ldez@users.noreply.github.com>

--- gofmt/golangci.go
+++ gofmt/golangci.go
@@ -4,12 +4,22 @@ import (
 	"bytes"
 	"fmt"
 	"go/ast"
+	"go/parser"
 	"go/printer"
-	"io/ioutil"
+	"os"
+	"strings"
 )
 
+// Run runs gofmt.
+// Deprecated: use RunRewrite instead.
 func Run(filename string, needSimplify bool) ([]byte, error) {
-	src, err := ioutil.ReadFile(filename)
+	return RunRewrite(filename, needSimplify, "")
+}
+
+// RunRewrite runs gofmt.
+// empty string `rewrite` will be ignored.
+func RunRewrite(filename string, needSimplify bool, rewriteRule string) ([]byte, error) {
+	src, err := os.ReadFile(filename)
 	if err != nil {
 		return nil, err
 	}
@@ -25,6 +35,11 @@ func Run(filename string, needSimplify bool) ([]byte, error) {
 
 	if needSimplify {
 		simplify(file)
+	}
+
+	file, err = rewriteFileContent(rewriteRule, file)
+	if err != nil {
+		return nil, err
 	}
 
 	res, err := format(fileSet, file, sourceAdj, indentAdj, src, printer.Config{Mode: printerMode, Tabwidth: tabWidth})
@@ -43,4 +58,35 @@ func Run(filename string, needSimplify bool) ([]byte, error) {
 	}
 
 	return data, nil
+}
+
+func rewriteFileContent(rewriteRule string, file *ast.File) (*ast.File, error) {
+	if rewriteRule == "" {
+		return file, nil
+	}
+
+	f := strings.Split(rewriteRule, "->")
+	if len(f) != 2 {
+		return nil, fmt.Errorf("rewrite rule must be of the form 'pattern -> replacement'\n")
+	}
+
+	pattern, err := parseExpression(f[0], "pattern")
+	if err != nil {
+		return nil, err
+	}
+
+	replace, err := parseExpression(f[1], "replacement")
+	if err != nil {
+		return nil, err
+	}
+
+	return rewriteFile(pattern, replace, file), nil
+}
+
+func parseExpression(s, what string) (ast.Expr, error) {
+	x, err := parser.ParseExpr(s)
+	if err != nil {
+		return nil, fmt.Errorf("parsing %s %s at %s\n", what, s, err)
+	}
+	return x, nil
 }
